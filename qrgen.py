372c1c29450bafdf6d67dd2524ad7eb99ed14e2a
Chris Davison <chris_davison@hotmail.com>
2021-12-12 10:57:41 +0000

    Added a few options to the python script to allow WEP etc.

--- qrgen.py
+++ qrgen.py
@@ -1,18 +1,36 @@
 # https://pypi.org/project/qrcode/
 # https://en.wikipedia.org/wiki/QR_code
+# https://github.com/zxing/zxing/wiki/Barcode-Contents
 
 import qrcode
 import math
 import argparse
 
-# Notes: Assumes WPA security and the SSID not to be hidden.
+# Notes:
+# - Pre-escape your SSID and/or password if you have any special characters in them. Special characters \ ; , " : should be escaped with a backslash \.
+# - Doesn't support WPA2-EAP.
+# - WEP and hidden SSIDs are now supported but untested.
+# - Always using low error correct for the QR code.
+# - Should scale the QR code up in size if needed.
 
 parser = argparse.ArgumentParser(description='Generate data for QR code for provided Wifi network into scad file.')
-parser.add_argument('SSID', help='The WiFi networks SSID')
-parser.add_argument('Password', help='The password for that SSDID')
+parser.add_argument('SSID', help='The WiFi networks SSID.')
+parser.add_argument('Password', help='The password for that SSDID.')
+parser.add_argument("--wep", help="Set if using WEP security. Defaults to WPA.", action="store_true")
+parser.add_argument("--hidden", help="Set if the SSID in question is hidden.", action="store_true")
+parser.add_argument("--quiet", help="Don't output anything to command line.", action="store_true")
 args = parser.parse_args()
 
-data = "WIFI:T:WPA;S:" + args.SSID + ";P:" + args.Password + ";;"
+security = "WPA"
+hidden = ""
+
+if args.wep:
+	security = "WEP"
+	
+if args.hidden:
+	hidden = "H:true"
+
+data = "WIFI:T:" + security + ";S:" + args.SSID + ";P:" + args.Password + ";" + hidden + ";"
 
 qr = qrcode.QRCode(
     version=1,
@@ -30,8 +48,9 @@ mylist = list(img.getdata())
 size = int(math.sqrt(len(mylist)))
 count = 0;
 
-print(data)
-print(size, 'x', size)
+if not args.quiet:
+	print(data)
+	print(size, 'x', size)
 
 f = open("data.scad", "w")
 
